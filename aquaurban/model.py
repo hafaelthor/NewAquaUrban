fe1ff04ec088184d1c72c6243450389e158cde0e
Hafael Thor <hafaelthor@gmail.com>
2019-01-19 18:11:49 -0300

    Redesign and Drop Database

--- aquaurban/model.py
+++ aquaurban/model.py
@@ -9,38 +9,40 @@ def load_user (user_id):
 	return User.query.get(int(user_id))
 
 class User (db.Model, UserMixin):
-	id 			= db.Column(db.Integer, primary_key=True)
-	username	= db.Column(db.String(20), unique=True, nullable=False)
-	email	 	= db.Column(db.String(50), unique=True, nullable=False)
-	password 	= db.Column(db.String(30), nullable=False)
-	permission	= db.Column(db.SmallInteger, nullable=False)
+	id 				= db.Column(db.Integer, 	primary_key=True)
+	username		= db.Column(db.String(20), 	unique=True, 	nullable=False)
+	email	 		= db.Column(db.String(50), 	unique=True, 	nullable=False)
+	password 		= db.Column(db.String(30), 					nullable=False)
+	permission		= db.Column(db.SmallInteger, 				nullable=False)
+	community_id	= db.Column(db.Integer, db.ForeignKey('community.id'))
 
-	systems 	= db.relationship('System', backref='user', lazy=True)
-	actions		= db.relationship('Action', backref='user', lazy=True)
+	systems 		= db.relationship('System', backref='user', lazy=True)
+	actions			= db.relationship('Action', backref='user', lazy=True)
 
 	def __repr__ (self):
 		return f'<USER {self.id} | username=\"{self.username}\" email=\"{self.email}\">'
 
 class Community (db.Model):
-	id 			= db.Column(db.Integer, primary_key=True)
-	host 		= db.Column(db.String(40), nullable=False)
-	port 		= db.Column(db.Integer, nullable=False)
+	id 			= db.Column(db.Integer, 	primary_key=True)
+	name		= db.Column(db.String(40), 	nullable=False)
+	host 		= db.Column(db.String(40), 	nullable=False)
+	port 		= db.Column(db.Integer, 	nullable=False)
 	username 	= db.Column(db.String(20))
 	password 	= db.Column(db.String(20))
 
 	systems		= db.relationship('System', backref='community', lazy=True)
 
 	def __repr__ (self):
-		return f'<COMMUNITY {self.id} | {self.host}:{self.port}>'
+		return f'<COMMUNITY {self.id} | {self.name} on {self.host}:{self.port}>'
 
 class System (db.Model):
-	id 				= db.Column(db.Integer, primary_key=True)
-	name 			= db.Column(db.String(20), nullable=False)
-	user_id			= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-	community_id	= db.Column(db.Integer, db.ForeignKey('community.id'), nullable=False)
+	id 				= db.Column(db.Integer, 								primary_key=True)
+	name 			= db.Column(db.String(20), 								nullable=False)
+	user_id			= db.Column(db.Integer, db.ForeignKey('user.id'), 		nullable=False)
+	community_id	= db.Column(db.Integer, db.ForeignKey('community.id'), 	nullable=False)
 
-	bioinfos		= db.relationship('Bioinfo', backref='system', lazy=True)
-	actions			= db.relationship('Action', backref='system', lazy=True)
+	bioinfos		= db.relationship('Bioinfo', 	backref='system', lazy=True)
+	actions			= db.relationship('Action', 	backref='system', lazy=True)
 
 	def safe_ids_for (self, feature):
 		ids = [self.user_id]
@@ -76,9 +78,9 @@ class Bioinfo (db.Model):
 		return f'<BIOINFO {self.id} | [{"above" if self.waterlevel else "below"} {self.brightness}lm {self.temperature}ºC pH({self.acidness})] system_id={self.system_id}>'
 
 class Action (db.Model):
-	id 			= db.Column(db.Integer, primary_key=True)
+	id 			= db.Column(db.Integer, 							primary_key=True)
 	timestamp	= db.Column(db.TIMESTAMP)
-	code		= db.Column(db.SmallInteger, nullable=False)
+	code		= db.Column(db.SmallInteger, 						nullable=False)
 	data		= db.Column(db.SmallInteger)
 	system_id 	= db.Column(db.Integer, db.ForeignKey('system.id'), nullable=False)
 	user_id 	= db.Column(db.Integer, db.ForeignKey('user.id'))
